9ca4fa22dd7f1457e4781d0f8d02f951388179cc
Sly <slyacezeta@gmail.com>
2025-04-15 22:07:32 -0400

    adding changelog, updating cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "v1";
+const CACHE = "2025-04-15-0";
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js");
 self.addEventListener("install", (event) => {
 	self.skipWaiting();
