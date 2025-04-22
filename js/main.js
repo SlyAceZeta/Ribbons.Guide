SlyAceZeta/Ribbons.Guide

commit d6400ec5487173f0a5a5a568b9088ee5f6d7ed6e

restoring title ribbon in card headers
closes #96

also fixing the Strange Ball not being correctly aligned, forgot to do this in commit 5ee6659 per #86

css/main.css
@@ -359,6 +359,12 @@ a:not(:hover):not(:focus) {
 	width: auto;
 	height: 20px;
 }
+.card-header-right {
+	height: 1.73rem;
+}
+.card-header .card-header-right * {
+	height: 100%;
+}
 .card-header button {
 	width: 28px;
 	background: url("../img/ui/clipboard.png") center center no-repeat;
@@ -467,6 +473,7 @@ html[data-cardview="condensed"] .card-ribbons,
 html[data-cardview="condensed"] .card-footer-top,
 html[data-cardview="condensed"] .card .card-header-title,
 html[data-cardview="condensed"] .card-header-gender,
+html[data-cardview="condensed"] .card-header-title-ribbon,
 html[data-cardview="condensed"] .card-footer-level,
 html[data-cardview="condensed"] .card-footer-language,
 html[data-cardview="condensed"] .card-header-shiny {

js/main.js
@@ -1087,7 +1087,7 @@ function createCard(p, id){
 	var $cardHeaderBallStrange = "";
 	if(p.currentgame && ((p.currentgame !== "pla" && p.currentgame !== "home" && balls[p.ball].hisui) || (p.currentgame == "pla" && !balls[p.ball].hisui))){
 		if(p.strangeball !== "disabled"){
-			$cardHeaderBallStrange = $("<img>", { "class": "align-middle me-2", "src": "img/balls/strange.png", "alt": "Strange Ball", "title": "Strange Ball" });
+			$cardHeaderBallStrange = $("<img>", { "class": "align-text-top me-2", "src": "img/balls/strange.png", "alt": "Strange Ball", "title": "Strange Ball" });
 			if(p.strangeball == ""){
 				$cardHeaderBallMain.addClass("card-header-ball-selected");
 				$cardHeaderBallStrange.addClass("card-header-ball-strange");
@@ -1152,10 +1152,21 @@ function createCard(p, id){
 		$cardHeaderLeft.append($("<img>", { "class": "align-text-top ms-2 card-header-shiny", "src": "img/ui/" + shinyIcon, "alt": "Shiny", "title": "Shiny" }));
 	}
 	$cardHeader.append($cardHeaderLeft);
+	
+	var $cardHeaderRight = $("<div>", { "class": "card-header-right d-flex" });
+	var $cardHeaderTitle = "";
+	if(p.title && p.title !== "None"){
+		var titleRibbonDir = "ribbons/";
+		if(ribbons[p.title].mark){
+			titleRibbonDir = "marks/";
+		}
+		$cardHeaderTitle = $("<img>", { "class": "ms-2 card-header-title-ribbon", "src": "img/" + titleRibbonDir + p.title + ".png", "alt": ribbons[p.title].names["eng"], "title": ribbons[p.title].names["eng"] + " - " + ribbons[p.title].descs["eng"] });
+	}
 	var $cardHeaderButton = $("<button>", { "type": "button", "class": "btn btn-link p-0 ms-1 position-relative", "onclick": "ribbonChecklist()" })
 		.append($("<span>", { "class": "ribbon-checklist-warning-badge position-absolute translate-middle bg-danger rounded-circle" }).html($("<span>", {"class": "visually-hidden"}).text("Warnings")));
+	$cardHeaderRight.append($cardHeaderTitle, $cardHeaderButton);
 
-	$cardHeader.append($cardHeaderButton);
+	$cardHeader.append($cardHeaderRight);
 
 	/* body */
 	var speciesSprite = p.species;

commit 0ec15e4e4511f729583ee54e4bc29b0d7868a192

adding Beldum/Metang Footprint Ribbon warning
- also fixing slight issue with warning bubble introduced in commit d6400ec
- also adding preference to open ribbons.guide links in PWA app if installed
- pushing live

css/main.css
@@ -453,13 +453,15 @@ html[data-autostrangeball="true"] .card-header-ball-strange {
 .card .btn-link img {
 	filter: var(--rg-icon-filter);
 }
-.ribbon-checklist-warning-badge {
+.card-header .card-header-right .ribbon-checklist-warning-badge {
 	display: none;
 	top: 25%;
 	left: 85%;
 	padding: 0.33rem;
+	width: 10px;
+	height: 10px;
 }
-.col[data-ribbon-warnings] .ribbon-checklist-warning-badge {
+.col[data-ribbon-warnings] .card-header .card-header-right .ribbon-checklist-warning-badge {
 	display: inline;
 }
 .card-sprite {

js/main.js
@@ -512,8 +512,9 @@ function getEarnableRibbons(dex, currentLevel, metLevel, currentGame, originGame
 						if(ribbonGen !== 4){
 							// if Pokemon is voiceless and can go to Gen VIII, it can always earn this
 							var voiceless = getPokemonData(dex, "voiceless");
-							if(!(ribbonGen == 8 && voiceless)){
+							if(!(ribbonGen == 8 && voiceless) || ((dex === "beldum" || dex === "metang") && (originGame === "scar" || originGame === "vio")) ){
 								// otherwise, Footprint relies on Met Level < 71
+								// Beldum and Metang are voiceless, but Metagross is not, and Beldum can be met as high as Lv.74 in SV DLC, so evolving to Metagross will also disqualify it
 								var currentLevelBelow71 = currentLevel < 71;
 								// Met Level changes upon entering Gen V or leaving Virtual Console
 								if(currentGen < 5 || virtualConsole){
@@ -537,7 +538,12 @@ function getEarnableRibbons(dex, currentLevel, metLevel, currentGame, originGame
 										// user has set Met Level
 										if(metLevel > 70){
 											// Pokemon was met at 71+, Footprint is unavailable
-											continue;
+											// Beldum and Metang can still get it, but they need a warning
+											if(dex === "beldum" || dex === "metang"){
+												earnableWarnings.push("footprint-beldum");
+											} else {
+												continue;
+											}
 										}
 									} else {
 										// user has not set Met Level, let's try to determine it automatically
@@ -545,9 +551,9 @@ function getEarnableRibbons(dex, currentLevel, metLevel, currentGame, originGame
 										if(originGame !== "go"){
 											// Pokemon in Gen V+ with Current Level < 71 must also have Met Level < 71 and can always earn Footprint
 											if(!currentLevelBelow71){
-												// we cannot automatically determine Met Level
+												// we cannot automatically determine Met Level, warn the user as such (including Beldum/Metang case)
 												// before we warn the user as such, let's check if the ribbon will appear for the Pokemon in BDSP--if so, no warning is necessary
-												if(!((compatibleGames.includes("bd") || compatibleGames.includes("sp")) && voiceless)){
+												if(!((compatibleGames.includes("bd") || compatibleGames.includes("sp")) && voiceless) || dex === "beldum" || dex === "metang"){
 													earnableWarnings.push("footprint-met-level");
 												}
 											}
@@ -883,7 +889,8 @@ function ribbonChecklist(){
 			if(ribbonWarnings[w] == "winning-ribbon") warningText = "If " + cardData.name + " reaches Lv.51, the Winning Ribbon will become unavailable!";
 			if(ribbonWarnings[w] == "footprint-gen4") warningText = "If " + cardData.name + " reaches Lv.71 before transferring to Gen&nbsp;V, the Footprint Ribbon will only be available in Gen&nbsp;IV!";
 			if(ribbonWarnings[w] == "footprint-virtualconsole") warningText = "If " + cardData.name + " reaches Lv.71 before transferring to Gen&nbsp;VII, the Footprint Ribbon will become unavailable!";
-			if(ribbonWarnings[w] == "footprint-met-level") warningText = cardData.name + "'s Met Level has not been set. The availability of the Footprint Ribbon after Gen&nbsp;IV cannot be determined.";
+			if(ribbonWarnings[w] == "footprint-met-level") warningText = cardData.name + "'s Met Level has not been set. The availability of the Footprint Ribbon cannot be determined.";
+			if(ribbonWarnings[w] == "footprint-beldum") warningText = "Evolving " + cardData.name + " into Metagross will make the Footprint Ribbon unavailable!";
 			if(ribbonWarnings[w] == "evolution-warning"){
 				var evoWarnName = getPokemonData(cardData.evolutionWarning, "names")["eng"];
 				var evoWarnForms = getPokemonData(cardData.evolutionWarning, "forms");

sw.js
@@ -1,4 +1,4 @@
-const CACHE = "2025-04-20-0";
+const CACHE = "2025-04-22-0";
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js");
 self.addEventListener("install", (event) => {
 	self.skipWaiting();