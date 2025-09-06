456fd1bdd896b2190f9fa2ef4c66ff5d889838b4
Sly <slyacezeta@gmail.com>
2025-09-06 14:45:02 -0400

    updating cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "2025-05-07-0";
+const CACHE = "2025-09-06-0";
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js");
 self.addEventListener("install", (event) => {
 	self.skipWaiting();
