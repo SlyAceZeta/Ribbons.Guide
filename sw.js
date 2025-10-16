c849f4eb0dc700182f7c4b3aadaf53fde3aee04b
Sly <slyacezeta@gmail.com>
2025-10-16 14:20:04 -0400

    pushing hotfix and CSS updates live

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "2025-10-15-0";
+const CACHE = "2025-10-16-0";
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/7.3.0/workbox-sw.js");
 self.addEventListener("install", (event) => {
 	self.skipWaiting();
