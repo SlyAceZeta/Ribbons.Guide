2e567b9988a280b4fdc5011b4648903ca7c1aa9e
Sly <slyacezeta@gmail.com>
2026-04-04 22:11:23 -0400

    bug fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "2026-04-02-0";
+const CACHE = "2026-04-04-0";
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/7.3.0/workbox-sw.js");
 self.addEventListener("install", (event) => {
 	self.skipWaiting();
