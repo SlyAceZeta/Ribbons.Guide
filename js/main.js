28e6ee433ccf18d87c4d7cc87c9673526cfc6ff1
Sly <slyacezeta@gmail.com>
2026-04-16 02:26:10 -0400

    minor improvements and cleanup
    - clearing master-rank-sv-2025 local storage if it still exists
    - cleaning up the Dropbox URL helper utility
    - adding helper utility to clear URL parameters
    - removing rudimentary nonfunctional Dropbox functions for now
    - updating backup load function to support a declared file version and to combine two separated sections
    - minor tab fix for themes.css

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /* globals */
-var balls, changelog, games, gameOrder = {}, gameGroups, importmap, origins, pokemon, ribbons, translations, forms, natures, modalSettings, modalRibbonChecklist, modalPokemonForm, modalPokemonState = "default", modalPokemonEditing = -1, activeFilters = {}, activeSort = "default", filterState = "default", offcanvasSelect, selectState = "off";
+var balls, changelog, games, gameOrder = {}, gameGroups, importmap, origins, pokemon, ribbons, translations, forms, natures, modalSettings, modalRibbonChecklist, modalPokemonForm, modalPokemonState = "default", modalPokemonEditing = -1, activeFilters = {}, activeSort = "default", filterState = "default", offcanvasSelect, selectState = "off", DROPBOX_CLIENT_ID = "xxvozybw2lp9ycy";
 // voiced BDSP species that can evolve into voiceless BDSP species
 const evolveVoicelessMap = {"caterpie": ["metapod"], "weedle": ["kakuna"], "venonat": ["venomoth"], "natu": ["xatu"], "larvitar": ["pupitar"], "wurmple": ["silcoon", "cascoon"], "bagon": ["shelgon"]};
 // voiceless BDSP species (and voiced BDSP species that can evolve into voiceless BDSP species) that can evolve into voiced BDSP species
@@ -7,7 +7,7 @@ const evolveVoicedMap = {"caterpie": ["butterfree"], "metapod": ["butterfree"], 
 // TODO: add tutorials
 /* clear old local storage properties if still present */
 /* except theme which gets special handling */
-var oldProps = ["site-update-warning"];
+var oldProps = ["site-update-warning", "master-rank-sv-2025"];
 for(var o in oldProps){
 	if(localStorage[oldProps[o]]){
 		localStorage.removeItem(oldProps[o]);
@@ -262,117 +262,41 @@ for(let i in toggles){
 	}
 }
 
-/* Dropbox */
+/* Dropbox URL helper utilities */
 (function (window) {
 	window.utils = {
 		parseQueryString(str) {
 			const ret = Object.create(null);
-			
-			if (typeof str !== 'string') {
-				return ret;
-			}
-			
+			if(typeof str !== "string") return ret;
 			str = str.trim().replace(/^(\?|#|&)/, '');
-			
-			if (!str) {
-				return ret;
-			}
-			
+			if(!str) return ret;
 			str.split('&').forEach((param) => {
 				const parts = param.replace(/\+/g, ' ').split('=');
-				// Firefox (pre 40) decodes `%3D` to `=`
-				// https://github.com/sindresorhus/query-string/pull/37
 				let key = parts.shift();
 				let val = parts.length > 0 ? parts.join('=') : undefined;
-				
 				key = decodeURIComponent(key);
-				
-				// missing `=` should be `null`:
-				// http://w3.org/TR/2012/WD-url-20120524/#collect-url-parameters
 				val = val === undefined ? null : decodeURIComponent(val);
-				
-				if (ret[key] === undefined) {
+				if(ret[key] === undefined){
 					ret[key] = val;
-				} else if (Array.isArray(ret[key])) {
+				} else if(Array.isArray(ret[key])){
 					ret[key].push(val);
 				} else {
 					ret[key] = [ret[key], val];
 				}
 			});
-			
 			return ret;
 		},
+		// remove token from URL bar so it isn't visible to the user
+		clearUrl(){
+			if(window.history && window.history.replaceState){
+				window.history.replaceState({}, document.title, window.location.origin + window.location.pathname);
+			} else {
+				window.location.search = "";
+				window.location.hash = "";
+			}
+		}
 	};
 }(window));
-
-var DROPBOX_CLIENT_ID = 'xxvozybw2lp9ycy';
-// Parses the url and gets the access token if it is in the urls hash
-function getDropboxTokenFromUrl() {
-	return utils.parseQueryString(window.location.hash).access_token;
-}
-function setDropboxTokenToLocalStorage(t) {
-	localStorage.DropboxToken = t;
-}
-function getDropboxTokenFromLocalStorage() {
-	return localStorage.DropboxToken;
-}
-function getDropboxToken() {
-	var DropboxToken = getDropboxTokenFromUrl();
-	if(DropboxToken){
-		return DropboxToken;
-	} else {
-		return getDropboxTokenFromLocalStorage();
-	}
-}
-// If the user was just redirected from authenticating, the urls hash will
-// contain the access token.
-function isAuthenticatedDropbox() {
-	return !!getDropboxToken();
-}
-// Render a list of items to #files
-/*function renderItems(items) {
-	var filesContainer = document.getElementById('files');
-	items.forEach(function(item) {
-		var li = document.createElement('li');
-		li.innerHTML = item.name;
-		filesContainer.appendChild(li);
-	});
-}*/
-$(function(){
-	if(localStorage.dropboxTest){
-		$("#dropboxTest").removeClass("d-none");
-		if (isAuthenticatedDropbox()) {
-			var DropboxToken = getDropboxTokenFromLocalStorage();
-			if(!DropboxToken){
-				DropboxToken = getDropboxTokenFromUrl();
-				setDropboxTokenToLocalStorage(DropboxToken);
-			}
-			
-			// Create an instance of Dropbox with the access token and use it to
-			// fetch and render the files in the users root directory.
-			var dbx = new Dropbox.Dropbox({ accessToken: DropboxToken });
-			dbx.filesListFolder({path: ''})
-			.then(function(response) {
-				$('#modalDataOnlineLoggedIn').removeClass("d-none");
-				//renderItems(response.result.entries);
-			})
-			.catch(function(error) {
-				console.error(error?.error || error);
-				$("#modalDataOnlineErrorText").text(error?.error?.error_summary || error?.error || error);
-				$("#modalDataOnlineError").removeClass("d-none");
-			});
-		} else {
-			$('#modalDataOnlineLogin').removeClass("d-none");
-			
-			// Set the login anchors href using dbx.getAuthenticationUrl()
-			var dbx = new Dropbox.Dropbox({ clientId: DROPBOX_CLIENT_ID });
-			var dbxAuthUrl = dbx.auth.getAuthenticationUrl('http://127.0.0.1:3000')
-			.then((dbxAuthUrl) => {
-				document.getElementById('modalDataOnlineLogin').href = dbxAuthUrl;
-			})
-		}
-	}
-});
 
 /* April Fools */
 var aprilFools = false;
@@ -519,16 +443,11 @@ function loadBackup(file, filename){
 					backupPokemon = JSON.parse(contents);
 				} else {
 					var backupObj = JSON.parse(contents);
-					if(backupObj.settings && backupObj.pokemon && backupObj.boxes){
+					if(backupObj.fileVersion){
+						fileVersion = backupObj.fileVersion;
+					} else if(backupObj.settings && backupObj.pokemon && backupObj.boxes){
 						fileVersion = 3;
-						if(backupObj.lastModified){
-							fileVersion = 4;
-							backupLastModified = backupObj.lastModified;
-						}
-						backupPokemon = backupObj.pokemon;
-						backupBoxes = backupObj.boxes;
-						settings = backupObj.settings;
-						localStorage.settings = JSON.stringify(backupObj.settings);
+						if(backupObj.lastModified) fileVersion = 4;
 					}
 				}
 			}
@@ -538,6 +457,12 @@ function loadBackup(file, filename){
 					for(let p in backupPokemon){
 						backupPokemon[p] = updateOldPokemon(backupPokemon[p]);
 					}
+				} else if(fileVersion >= 3){
+					backupPokemon = backupObj.pokemon;
+					backupBoxes = backupObj.boxes;
+					settings = backupObj.settings;
+					localStorage.settings = JSON.stringify(backupObj.settings);
+					if(fileVersion >= 4) backupLastModified = backupObj.lastModified;
 				}
 				localStorage.pokemon = JSON.stringify(backupPokemon);
 				localStorage.boxes = JSON.stringify(backupBoxes);
